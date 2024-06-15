284bb0c3d6bc1d6dd4478d5396ef3a5b3f18416a
DosX <admin@kay-software.ru>
2024-06-15 21:36:27 +0300

    Linux, PublishTrimmed=false

--- source/build_linux.cmd
+++ source/build_linux.cmd
@@ -1,2 +1,2 @@
-dotnet publish -c Release -r linux-x64 /p:TrimmerRootAssembly=true /p:TrimmerScanAssemblyFiles=true /p:PublishSingleFile=true -p:PublishAot=false -o build_linux_x64
+dotnet publish -c Release -r linux-x64 --self-contained true /p:PublishSingleFile=true /p:PublishAot=false /p:PublishTrimmed=false /p:TrimMode=copyused -o build_linux_x64\
 call cleanup
